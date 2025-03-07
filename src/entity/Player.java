0f4ab75f011fb1c15a7e0f0f01c6f33be6792476
Alexander Robles <ajmr0000@gmail.com>
2025-03-07 00:08:05 -0800

    AR - Implemented collision for desired tiles.

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -29,7 +29,11 @@ public class Player extends Entity{
 		screenX = gp.screenWidth / 2 - (gp.tileSize / 2);
 		screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
 		
-		solidArea = new Rectangle(0, 0, gp.tileSize, gp.tileSize);
+		solidArea = new Rectangle();
+		solidArea.x = gp.tileSize / 6;
+		solidArea.y = gp.tileSize / 3;
+		solidArea.width = gp.tileSize - (gp.tileSize / 3);
+		solidArea.height = gp.tileSize - (gp.tileSize / 3);
 		
 		setDefaultValues();
 		getPlayerImage();
@@ -80,22 +84,32 @@ public class Player extends Entity{
 			
 			if (keyH.upPressed == true) {
 				direction = "up";
-				worldY -= speed;
 			}
 			
 			else if (keyH.downPressed == true) {
 				direction = "down";
-				worldY += speed;
 			}
 			
 			else if (keyH.leftPressed == true) {
 				direction = "left";
-				worldX -= speed;
 			}
 			
 			else if (keyH.rightPressed == true) {
 				direction = "right";
-				worldX += speed;
+			}
+			
+			// CHECK TILE COLLISION
+			collisionOn = false;
+			gp.cChecker.checkTile(this);
+			
+			// IF COLLISION IS FALSE, PLAYER CAN MOVE
+			if(collisionOn == false) {
+				switch(direction) {
+					case "up": worldY -= speed; break;
+					case "down": worldY += speed; break;
+					case "left": worldX -= speed; break;
+					case "right": worldX += speed; break;
+				}
 			}
 			
 			afkCounter = 0;
@@ -111,7 +125,7 @@ public class Player extends Entity{
 		else {
 			afkCounter++;
 			
-			if (afkCounter > 180) {
+			if (afkCounter > 20) {
 				direction = "idle";
 			}
 		}
